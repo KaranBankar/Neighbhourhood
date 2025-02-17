c40a37896f8e9f7bd560ccd87333ac9798b9a66a
KaranBankar <karanbankar54@gmail.com>
2025-02-17 05:33:19 +0530

    Soem things done

--- app/src/main/java/com/example/neighbourhood/UserHomeActivity.java
+++ app/src/main/java/com/example/neighbourhood/UserHomeActivity.java
@@ -1,13 +1,18 @@
 package com.example.neighbourhood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.navigation.NavigationView;
 
 public class UserHomeActivity extends AppCompatActivity {
@@ -16,12 +21,50 @@ public class UserHomeActivity extends AppCompatActivity {
     private NavigationView navView;
     private Toolbar toolbar;
 
+    private MaterialCardView mainteinance,events,helpdesk,complain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_home); // Ensure this matches your XML file name
 
+        mainteinance=findViewById(R.id.maintainance);
+        events=findViewById(R.id.events);
+        helpdesk=findViewById(R.id.helpdesk);
+        complain=findViewById(R.id.complain);
+
+        mainteinance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  i=new Intent(UserHomeActivity.this,UserMaintenanceActivity.class);
+                startActivity(i);
+            }
+        });
+
+        events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  i=new Intent(UserHomeActivity.this, UserEventsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        helpdesk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  i=new Intent(UserHomeActivity.this,UserHelpdeskActivity.class);
+                startActivity(i);
+            }
+        });
+
+        complain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  i=new Intent(UserHomeActivity.this,UserComplainActivity.class);
+                startActivity(i);
+            }
+        });
         // Initialize views
         drawerLayout = findViewById(R.id.main);
         toolbar = findViewById(R.id.toolbar);
